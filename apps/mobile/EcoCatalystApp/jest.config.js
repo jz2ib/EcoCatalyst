0a8956c9da5b6857b90ffd0002853f97e35eddb2
El Patron Patron <jazibforwork@gmail.com>
2025-05-19 02:47:55 +0000

    done

--- apps/mobile/EcoCatalystApp/jest.config.js
+++ apps/mobile/EcoCatalystApp/jest.config.js
@@ -1,32 +1,9 @@
-module.exports = {
+/** @type {import('jest').Config} */
+export default {
   preset: 'jest-expo',
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'
+  'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'
   ],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  collectCoverageFrom: [
-    'src/**/*.{ts,tsx}',
-    '!src/**/*.d.ts',
-    '!src/**/types.ts',
-    '!src/theme/theme.ts',
-    '!src/services/firebase.ts',
-    '!**/node_modules/**'
-  ],
-  coverageThreshold: {
-    global: {
-      statements: 80,
-      branches: 80,
-      functions: 80,
-      lines: 80
-    }
-  },
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '/android/',
-    '/ios/'
-  ],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  moduleNameMapper: {
-    "^react-native/Libraries/Animated/NativeAnimatedHelper$": "<rootDir>/node_modules/react-native/src/private/animated/NativeAnimatedHelper.js"
-  }
-};
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  setupFiles: ['./jest.setup.js']
+  };
